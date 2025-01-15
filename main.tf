2bd2e55d13d08a2295e1f468175227fddb6431f2
Melisa ACAR <melisa.acar@ipa.istanbul>
2025-01-15 15:37:23 +0300

    feat: new structure added policy change needed

--- main.tf
+++ main.tf
@@ -1,73 +1,102 @@
 provider "aws" {
-    region = "eu-north-1"
+  region = var.region
 }
 
-# VPC
+locals {
+  cluster_name = "1-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
-    version = "~> 3.19.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
 
-    name = "eks-vpc"
-    cidr = "10.0.0.0/16"
+  name = "eks-vpc"
 
-    azs = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
-    public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  cidr = "10.0.0.0/16"
 
-    #public_subnet_tags = {
-    #    "kubernetes.io/role/elb" = "1"
-    #}
+  azs = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-    #private_subnet_tags = {
-    #    "kubernetes.io/role/internal-elb" = "1"     
-    #}
-    
-    #map_public_ip_on_launch = true # Public IP for Public Subnet
-    enable_nat_gateway = false
-    enable_vpn_gateway = false
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
+  enable_dns_hostnames = false
 
-        tags = {
-        Terraform = "true"
-        Environment = "dev"
-    }
-}
-
-# Internet Gateway 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = module.vpc.vpc_id
-}
-
-#  Route Table for Public Subnets
-resource "aws_route_table" "public" {
-  vpc_id = module.vpc.vpc_id
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id            = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public" {
-  for_each      = toset(module.vpc.public_subnets)
-  subnet_id     = each.value
-  route_table_id = aws_route_table.public.id
-}
-
-# EKS Cluster 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "eks-cluster-1"
-  version = "~> 20.31"
-
-  # Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
-  
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
+  # Adds labels to subnets. Important for Kubernetes load balancers.
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
   }
 
-  subnet_ids         = module.vpc.public_subnets
-  vpc_id          = module.vpc.vpc_id
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = local.cluster_name # Unique EKS cluster name taken from local variable.
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access           = true # Provides access to the EKS API from the public internet.
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }
